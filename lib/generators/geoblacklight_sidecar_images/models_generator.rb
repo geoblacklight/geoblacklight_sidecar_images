91c73f44c693f97bb122e2cb20fa7f5f2bb48a46
Eric Larson <ewlarson@umn.edu>
2018-01-25 11:00:02 -0600

    Add: WmsRewriteConcern

--- lib/generators/geoblacklight_sidecar_images/models_generator.rb
+++ lib/generators/geoblacklight_sidecar_images/models_generator.rb
@@ -10,19 +10,14 @@ module GeoblacklightSidecarImages
 
     desc <<-EOS
       This generator makes the following changes to your application:
-       1. Injects Geoblacklight into SolrDocument
+       1. Preps engine migrations
        2. Adds SolrDocumentSidecar ActiveRecord model
+       3. Adds WmsRewriteConcern
     EOS
 
     # Setup the database migrations
     def copy_migrations
       rake 'geoblacklight_sidecar_images_engine:install:migrations'
-    end
-
-    def include_wms_rewrite_solrdocument
-      inject_into_file 'app/models/solr_document.rb', after: 'include Geoblacklight::SolrDocument' do
-        "\n include WmsRewriteConcern"
-      end
     end
 
     def include_sidecar_solrdocument
@@ -39,7 +34,24 @@ module GeoblacklightSidecarImages
     end
 
     def create_solr_document_sidecar
-      copy_file 'models/solr_document_sidecar.rb', 'app/models/solr_document_sidecar.rb'
+      copy_file(
+        'models/solr_document_sidecar.rb',
+        'app/models/solr_document_sidecar.rb'
+      )
+    end
+
+    def create_wms_rewrite_concern
+      copy_file(
+        'models/concerns/wms_rewrite_concern.rb',
+        'app/models/concerns/wms_rewrite_concern.rb'
+      )
+    end
+
+
+    def include_wms_rewrite_solrdocument
+      inject_into_file 'app/models/solr_document.rb', after: 'include Geoblacklight::SolrDocument' do
+        "\n include WmsRewriteConcern"
+      end
     end
   end
 end
