092545e7c5cb69fdec1b9167ff51f77de258d4b9
Eric Larson <ewlarson@umn.edu>
2018-01-25 10:11:42 -0600

    Template

---
+++ template.rb
@@ -0,0 +1,14 @@
+# $ rails new app-name -m https://raw.githubusercontent.com/ewlarson/geoblacklight_sidecar_images/master/template.rb
+
+
+gem 'blacklight', '>= 6.3'
+gem 'geoblacklight', '>= 1.4'
+gem 'geoblacklight_sidecar_images', github: 'ewlarson/geoblacklight_sidecar_images', branch: 'master'
+
+run 'bundle install'
+
+generate 'blacklight:install', '--devise'
+generate 'geoblacklight:install', '-f'
+generate 'geoblacklight_sidecar_images:install', '-f'
+
+rake 'db:migrate'
