54dba9f4b70ec03cf314406880036b3906b418df
Eric Larson <ewlarson@umn.edu>
2018-01-25 11:42:29 -0600

    Fix: more active_storage to comment out

--- spec/test_app/config/application.rb
+++ spec/test_app/config/application.rb
@@ -5,7 +5,7 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
-require "active_storage/engine"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
@@ -26,4 +26,3 @@ module TestApp
     # -- all .rb files in that directory are automatically loaded.
   end
 end
-
