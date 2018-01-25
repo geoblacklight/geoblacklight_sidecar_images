6f30682e7a277699470bd69918cf46cdffab4713
Eric Larson <ewlarson@umn.edu>
2018-01-25 11:48:51 -0600

    Fix: correct gem name

--- spec/test_app/config/application.rb
+++ spec/test_app/config/application.rb
@@ -14,12 +14,12 @@ require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
-require "test_gem"
+require "geoblacklight_sidecar_images"
 
 module TestApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
