dd633c35c4cea0fcaf3535e05da62c4936adbf31
Eric Larson <ewlarson@umn.edu>
2018-03-07 09:13:37 -0600

    RuboCop: pay taxes

--- lib/generators/geoblacklight_sidecar_images/install_generator.rb
+++ lib/generators/geoblacklight_sidecar_images/install_generator.rb
@@ -2,7 +2,7 @@ require 'rails/generators'
 
 module GeoblacklightSidecarImages
   class Install < Rails::Generators::Base
-    source_root File.expand_path('../templates', __FILE__)
+    source_root File.expand_path('templates', __dir__)
 
     desc 'Install GeoblacklightSidecarImages'
 
