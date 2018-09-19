ea6ea5a1f5dbcbaedf3aea5d30bef71dd3cb01c0
Eric Larson <ewlarson@umn.edu>
2018-09-19 10:41:03 -0500

    Template: require statesman

--- template.rb
+++ template.rb
@@ -2,6 +2,7 @@
 
 gem 'blacklight', '>= 6.3'
 gem 'geoblacklight', '>= 1.4'
+gem 'statesman', '>= 3.4'
 gem 'geoblacklight_sidecar_images', :git => "git://github.com/ewlarson/geoblacklight_sidecar_images.git", :branch => "feature/rails-5.2"
 
 run 'bundle install'
