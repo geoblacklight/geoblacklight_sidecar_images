7b870b1f7889f0c8c9390cd6f9b2b6f2205303a9
Eric Larson <ewlarson@umn.edu>
2018-11-14 11:48:23 -0600

    Rubocop

--- spec/jobs/store_image_job_spec.rb
+++ spec/jobs/store_image_job_spec.rb
@@ -10,9 +10,7 @@ describe StoreImageJob, type: :job do
 
     it 'stores an image' do
       ActiveJob::Base.queue_adapter = :test
-      expect {
-        StoreImageJob.perform_later(document.id)
-      }.to have_enqueued_job(StoreImageJob)
+      expect { StoreImageJob.perform_later(document.id) }.to have_enqueued_job(StoreImageJob)
     end
   end
 end
